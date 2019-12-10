3911784264e8dc0ca368a609f3674486184b6790
Eugene van der Pijll <eugene@vanderpijll.nl>
2019-12-10 23:13:01 +0100

    Register marks on tile pages

--- lib/Output.py
+++ lib/Output.py
@@ -8,6 +8,31 @@ from Draw import LineStyle, FillStyle, TextStyle
 import Font
 
 
+def tile_register_marks(page, margin, tile):
+    # Horizontal lines
+    y = margin
+    while y <= page.height - margin:
+        Draw.line(page, (0, y), (page.width, y), LineStyle(Colour.black, 1))
+        y += tile.height
+
+    # Diagonal lines
+    x_top = margin + tile.width/4 + margin/3**.5
+    x_bottom = x_top - page.height/3**.5
+    while x_bottom <= page.width - margin:
+        Draw.line(page, (x_top, 0), (x_bottom, page.height), LineStyle(Colour.black, 1))
+        x_top += tile.width
+        x_bottom += tile.width
+
+    x_top = margin + tile.width*(page.height//tile.width + 3/4) - margin/3**.5
+    x_bottom = x_top + page.height/3**.5
+    while x_bottom >= margin:
+        Draw.line(page, (x_top, 0), (x_bottom, page.height), LineStyle(Colour.black, 1))
+        x_top -= tile.width
+        x_bottom -= tile.width
+
+    Draw.rectangle(page, (margin-2*mm, margin-2*mm), page.width - 2*margin + 4*mm, page.height - 2*margin + 4*mm,
+                   FillStyle(Colour.white))
+
 class Output:
     paper_width = A4_WIDTH
     paper_height = A4_HEIGHT
@@ -55,6 +80,7 @@ class Output:
         current_x_left = self.margin
         current_y_top = self.margin
         current_line = 0
+        tile_register_marks(page, self.margin, self.game.tiles[0])
         for tile in self.game.tiles:
             if current_x_left + tile.width >= self.document.width - self.margin:
                 current_y_top += tile.height
@@ -62,6 +88,7 @@ class Output:
                 current_x_left = self.margin + tile.width * (current_line % 2) / 2
                 if current_y_top + tile.height >= self.document.height - self.margin:
                     page = self.document.new_page()
+                    tile_register_marks(page, self.margin, tile)
                     current_x_left = self.margin
                     current_y_top = self.margin
                     current_line = 0
